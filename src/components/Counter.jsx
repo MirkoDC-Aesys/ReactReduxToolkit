2e12bdf628cbdde43e7d898188f3f35c301a2896
MirkoDC <77012574+MirkoDC@users.noreply.github.com>
2021-07-04 10:43:12 +0200

    add logic to counter component

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import Button from './Button'
+import { useSelector, useDispatch } from 'react-redux'
+import { counterSelector } from '../slices/counter'
+import { incremented, decremented } from '../slices/counter'
 
 export default function Counter() {
+    const dispatch = useDispatch()
+    const count = useSelector(counterSelector)
+
     return (
         <section>
             <h2>Counter</h2>
             <div>
-                <Button>-</Button>
-                <span>0</span>
-                <Button>+</Button>
+                <Button onClick={() => dispatch(decremented())}>-</Button>
+                <span>{count}</span>
+                <Button onClick={() => dispatch(incremented())}>+</Button>
             </div>
         </section>
     )
