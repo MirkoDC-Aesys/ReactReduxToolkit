83f8fd8cffeff6dad4e044713322ead21d3b3493
MirkoDC-Aesys <85115982+MirkoDC-Aesys@users.noreply.github.com>
2021-07-05 13:11:59 +0200

    add sagaMiddleware

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 import rootReducer from './slices'
+import createSagaMiddleware from 'redux-saga';
+import { watcherSaga } from './sagas/rootSaga';
+
+const sagaMiddleware = createSagaMiddleware()
 
 const store = configureStore({
-  reducer: rootReducer
+  reducer: rootReducer,
+  middleware: [...getDefaultMiddleware({ thunk: false }), sagaMiddleware]
 })
+
+sagaMiddleware.run(watcherSaga)
 
 ReactDOM.render(
   <React.StrictMode>
